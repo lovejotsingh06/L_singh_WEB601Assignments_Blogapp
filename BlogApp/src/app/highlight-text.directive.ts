963bfa40c6d7e7ae0dde2bedb7a7e2ac5d76939e
lovejotsingh06 <133062126+lovejotsingh06@users.noreply.github.com>
2024-02-05 10:30:02 -0500

    update

---
+++ BlogApp/src/app/highlight-text.directive.ts
@@ -0,0 +1,11 @@
+import { Directive } from '@angular/core';
+
+@Directive({
+  selector: '[appHighlightText]',
+  standalone: true
+})
+export class HighlightTextDirective {
+
+  constructor() { }
+
+}
