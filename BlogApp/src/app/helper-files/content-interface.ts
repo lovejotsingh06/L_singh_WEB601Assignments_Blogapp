bb26c24434841af10cbe236355a8bb739f2cefea
lovejotsingh06 <w0829892@myscc.ca>
2024-03-11 15:58:34 -0400

    5

--- BlogApp/src/app/helper-files/content-interface.ts
+++ BlogApp/src/app/helper-files/content-interface.ts
@@ -1,9 +1,11 @@
+//define an interface with the following parameters
+//Note: ? is optional
 export interface Content {
-  id: number;
-  title: string;
-  description: string;
-  creator: string;
-  imgURL?: string;
-  type?: string;
-  tags?: string[];
+    id: number;
+    title: string;
+    description: string;
+    creator: string;
+    imgURL?: string;
+    type?: string;
+    tags?: string[];
 }
